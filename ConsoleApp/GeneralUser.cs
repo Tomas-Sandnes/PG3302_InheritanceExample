e4adac8e4560ebf2b67ff3fbf74fddb8ab59c314
Tomas-Sandnes <tomas.sandnes@commodore.no>
2026-09-08 12:54:09 +0200

    Added 2 user classes.

---
+++ ConsoleApp/GeneralUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class GeneralUser
+    {
+        public string Name { get; set; }
+
+        public GeneralUser(string name)
+        {
+            Name = name;  
+        }
+
+        public void Contact()
+        {
+            Console.WriteLine($"Contacting {Name} by email.");
+        }
+    }
+}
---
+++ ConsoleApp/MessengerUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class MessengerUser : GeneralUser
+    {
+        public MessengerUser(string name) : base(name)
+        {
+            // Intentionally left blank.
+        }
+    }
+}
